ac8ef9a3eb87137008d3b0ef40928e6b2e0b082d
Shanmugham <shanmughams@gmail.com>
2022-10-16 18:29:59 +0530

    Multi module

---
+++ pet-clinic-data/src/test/java/com/shan/spring5/sfg/sfgpetclinic/PetClinicDataApplicationTests.java
@@ -0,0 +1,18 @@
+package com.shan.spring5.sfg.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+//@SpringBootTest(classes = PetClinicDataApplication.class)
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PetClinicDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
